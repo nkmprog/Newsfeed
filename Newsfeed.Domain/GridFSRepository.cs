2c2769c5866bcd25cd46cfc3fe8b555ba533a627
Georgi Mateev <gmmateev92@abv.bg>
2014-02-08 17:58:41 +0200

    Added GridFS repository

---
+++ Newsfeed.Domain/GridFSRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Newsfeed.Domain
+{
+    public class GridFSRepository
+    {
+        public GridFSRepository()
+        {
+            this.database = Database.GetDB();
+        }
+
+        public ObjectId UploadFile(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                var gridFsInfo = database.GridFS.Upload(fs, fileName);
+                var fileId = gridFsInfo.Id;
+
+                return  (ObjectId)fileId;
+            }
+        }
+
+        private readonly MongoDatabase database;
+    }
+}
