f9d3b747fc4b8ba1381b8b6a377c6ec1f2b145ed
nkmprog <nkm.work@gmail.com>
2014-02-08 15:15:34 +0200

    Data access layer
    Done initial work on the Domain and Data access layer.

---
+++ Newsfeed.Domain/MessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newsfeed.Domain
+{
+    class MessageRepository
+    {
+        private MongoCollection<Message> messageCollection;
+
+    }
+}
